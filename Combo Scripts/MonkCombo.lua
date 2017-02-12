b6059eafe20ff37272471e90a8ec6d1d5682a758
Gustavo <xzxgac@hotmail.com>
2017-02-11 19:58:02 -0600

    Updated Strong to Ultimate Kicks

--- Combo Scripts/MonkCombo.lua
+++ Combo Scripts/MonkCombo.lua
@@ -21,10 +21,10 @@ healers[3] = {Words = "Prayer", Exaus = "exura ico", Health = 70}
 healers[4] = {Words = "exura san", Exaus = "exura san", Health = 90}
 
 kick = {} -- HOW-TO; Put 1, 2 and 3 to the 3 kicks you want to use YOU'RE NOT going to use kick 4 and 5, so make sure you config it RIGHT.
-kick[1] = {Words = "Ultimate Death Kick", Exaus ="exori max mort"}
+kick[1] = {Words = "Ultimate Energy Kick", Exaus ="exori max vis"}
 kick[2] = {Words = "Ultimate Ice kick", Exaus ="exori max frigo"}
 kick[3] = {Words = "Ultimate Earth Kick", Exaus ="exori max tera"}
-kick[4] = {Words = "Ultimate Energy Kick", Exaus ="exori max vis"}
+kick[4] = {Words = "Ultimate Death Kick", Exaus ="exori max mort"}
 kick[5] = {Words = "Ultimate Fire Kick", Exaus ="exori max flam"}
 
 
